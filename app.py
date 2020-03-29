28417a01e5ab40b8c554e90c6afacebcbc9448a0
MalC-U <mwcalow@gmail.com>
2020-03-29 20:30:02 +0100

    Created app.py file that contains flask app &connects to the DB

--- app.py
+++ app.py
@@ -3,4 +3,14 @@ from flask import Flask
 from os import path
 
 if path.exists("env.py"):
-  import env 
+  import env
+  
+app = Flask(__name__)
+app.config['MONGO_DBNAME'] = os.getenv("MONGO_DBNAME")
+app.config['MONGO_URI'] = os.getenv("MONGODB_URI")
+
+
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+    port=os.environ.get('PORT'),
+    debug=True)
