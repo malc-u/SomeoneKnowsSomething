88423b7b2cc234ccbd5cfa8bcf2ecc97113bc607
MalC-U <mwcalow@gmail.com>
2020-04-02 21:36:56 +0100

    Amended route for index.html in order to read database

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import os
 from flask import Flask, render_template, url_for
+from flask_pymongo import PyMongo
 from os import path
+
 
 if path.exists("env.py"):
   import env
@@ -9,11 +11,14 @@ app = Flask(__name__)
 app.config['MONGO_DBNAME'] = os.getenv("MONGO_DBNAME")
 app.config['MONGO_URI'] = os.getenv("MONGODB_URI")
 
+mongo = PyMongo(app)
+
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html')
-
+  return render_template('index.html', 
+  podcasts = mongo.db.podcasts.find({"is_recommended": True}),
+  favourtites = mongo.db.podcasts.find({"is_favourite": True}))
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
