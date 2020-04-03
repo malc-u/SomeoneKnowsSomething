6fc40dd6c517e4573fb5d6c579e6c503d14980be
MalC-U <mwcalow@gmail.com>
2020-04-03 18:12:54 +0100

    imported registration form and Login form from forms.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import os
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, redirect, request, session
 from flask_pymongo import PyMongo
+from werkzeug.security import generate_password_hash, check_password_hash
+from forms import RegistrationForm, LoginForm
 from os import path
 
 
@@ -10,14 +12,26 @@ if path.exists("env.py"):
 app = Flask(__name__)
 app.config['MONGO_DBNAME'] = os.getenv("MONGO_DBNAME")
 app.config['MONGO_URI'] = os.getenv("MONGODB_URI")
+app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
 
 mongo = PyMongo(app)
 
 @app.route('/')
-@app.route('/index'):
+@app.route('/index')
 def index():
   return render_template('index.html')
-  
+
+@app.route('/register')
+def register():
+  form_register = RegistrationForm()
+  return render_template('register.html', title = 'Register', form = form_register )
+
+@app.route('/login')
+def login():
+  form_login = LoginForm()
+  return render_template('login.html', title = 'Login', form = form_login )
+
+
 @app.route('/recommended')
 def recommended():
   return render_template('recommended.html', 
