a58a2fe043e721bc11206cf4b61efd3fa7e2d406
MalC-U <mwcalow@gmail.com>
2020-04-04 19:52:07 +0100

    amended max username length

--- forms.py
+++ forms.py
@@ -3,7 +3,7 @@ from wtforms import StringField, PasswordField, SubmitField, SelectField, Boolea
 from wtforms.validators import DataRequired, Length, EqualTo
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Username:', validators=[DataRequired(), Length(min=5, max=15)])
+    username = StringField('Username:', validators=[DataRequired(), Length(min=5, max=20)])
     password = PasswordField('Password:', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password:', validators=[DataRequired(), 
                                                                     EqualTo('password', 
