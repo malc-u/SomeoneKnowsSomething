ecbbd60ddad13c184f753af4a2641e72291d5cb0
MalC-U <mwcalow@gmail.com>
2020-04-11 18:45:40 +0100

    Amended registration and login form labels

--- forms.py
+++ forms.py
@@ -1,16 +1,17 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, SelectField, BooleanField
+from wtforms import StringField, PasswordField, SubmitField, RadioField, BooleanField
 from wtforms.validators import DataRequired, Length, EqualTo
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Username:', validators=[DataRequired(), Length(min=5, max=20)])
-    password = PasswordField('Password:', validators=[DataRequired()])
-    confirm_password = PasswordField('Confirm Password:', validators=[DataRequired(), 
+    username = StringField('Username: ', validators=[DataRequired(), Length(min=5, max=20)])
+    password = PasswordField('Password: ', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password: ', validators=[DataRequired(), 
                                                                     EqualTo('password', 
                                                                     message='Passwords must match.')])
     submit = SubmitField('Register')
 
 class LoginForm(FlaskForm):
-    username = StringField('Username:', validators=[DataRequired()])
-    password = PasswordField('Password:', validators=[DataRequired()]) 
-    submit = SubmitField('Login:')
+    username = StringField('Username: ', validators=[DataRequired()])
+    password = PasswordField('Password: ', validators=[DataRequired()]) 
+    submit = SubmitField('Login')
+
